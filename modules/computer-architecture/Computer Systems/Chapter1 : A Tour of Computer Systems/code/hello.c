e273ecfbe4ab2efa444479acfed18ad99984249c
MingChen <25345174+icm-ai@users.noreply.github.com>
2025-12-11 11:35:55 +0800

    feat: update .gitignore to include additional file types and add hello.c example

---
+++ modules/computer-architecture/Computer Systems/Chapter1 : A Tour of Computer Systems/code/hello.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: MingChen
+ * @Date: 2025-12-10 22:43:37
+ * @LastEditors: MingChen
+ * @LastEditTime: 2025-12-10 22:47:06
+ */
+# include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n");
+    return 0;
+}
